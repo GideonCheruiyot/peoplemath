0ad5218a8786d15f701032cc7194ba34900bbec7
Andrew Medworth <github@medworth.org.uk>
2021-01-24 15:36:27 +0000

    RenameClassDialog -> RenameClassDialogComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,7 +48,7 @@ import { BucketSummaryComponent } from './bucket-summary/bucket-summary.componen
 import { GroupSummaryComponent } from './group-summary/group-summary.component';
 import { TagSummaryComponent } from './tag-summary/tag-summary.component';
 import { ResourceQuantityComponent } from './resource-quantity/resource-quantity.component';
-import { RenameClassDialog } from './rename-class-dialog/rename-class-dialog.component';
+import { RenameClassDialogComponent } from './rename-class-dialog/rename-class-dialog.component';
 import { PillComponent } from './pill/pill.component';
 import { LoginComponent } from './login/login.component';
 import {NotificationService} from './services/notification.service';
@@ -83,7 +83,7 @@ import {ModalComponent} from './modal/modal.component';
     GroupSummaryComponent,
     TagSummaryComponent,
     ResourceQuantityComponent,
-    RenameClassDialog,
+    RenameClassDialogComponent,
     PillComponent,
     LoginComponent,
     ModalComponent,
